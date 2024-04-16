9a9ee799a800c90eaed34dd17c439dafb0a18bd9
alex <alexg89@vt.edu>
2024-04-15 23:19:57 -0700

    adds simple analysis json wf example pt 1

--- mockingbird/mockingbooks_py/prototype/bootstrap.py
+++ mockingbird/mockingbooks_py/prototype/bootstrap.py
@@ -1,6 +1,8 @@
 import json
 
-from mockingbird.mockingbooks_py.schemas import create_or_update_schema
+from mockingbird.mockingbooks_py.analysis_tasks import create_analysis_task, get_task_id_by_name, get_task_by_name
+from mockingbird.mockingbooks_py.schemas import create_or_update_schema, get_schema_by_name
+from mockingbird.mockingbooks_py.workflows import create_wf
 
 
 def schema_create():
@@ -9,5 +11,28 @@ def schema_create():
     create_or_update_schema(data)
 
 
+def analysis_create():
+    with open('mocks/analysis.json', 'r') as file:
+        data = json.load(file)
+    data['schemas'] = [get_schema_by_name('profile-data-extraction')]
+    create_analysis_task(data)
+
+
+def workflow_create():
+    with open('mocks/workflow.json', 'r') as file:
+        jdata = json.load(file)
+
+    jdata['stepSize'] = 10
+    jdata['stepSizeUnit'] = 'minutes'
+
+    # Add a task to the workflow
+    task_str_id = get_task_id_by_name('profile-extraction')
+    jdata['models'][task_str_id] = get_task_by_name('profile-extraction')
+
+    pretty_data = json.dumps(jdata, indent=4)
+    print(pretty_data)
+    create_wf(jdata)
+
+
 if __name__ == '__main__':
-    schema_create()
+    workflow_create()
